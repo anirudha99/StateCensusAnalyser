51e232778e11375254da4f637e06d7256a96e57b
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-30 10:39:43 +0530

    Added use case with test cases

--- src/main/java/com/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/censusanalyser/StateCensusAnalyser.java
@@ -1,6 +1,7 @@
 package com.censusanalyser;
 
 import java.io.IOException;
+
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
@@ -22,28 +23,43 @@ public class StateCensusAnalyser {
 		this.Sample_CSV_File_Path = Sample_CSV_File_Path;
 	}
 
-	public int readStateRecordDetail() throws MyException {
+	/**
+	 * @param <T>
+	 * @param className - Name of the class to which we wanna map csv file data to
+	 * @return number of data read from csv file
+	 * @throws MyException
+	 */
+	public <T> int readStateRecordDetail(Class<T> className) throws MyException {
 		int count = 0;
 
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(Sample_CSV_File_Path));
 			@SuppressWarnings("unchecked")
-			CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader).withType(CSVStateCensus.class)
-			.withIgnoreLeadingWhiteSpace(true).build();
-			Iterator<CSVStateCensus> csvUserIterator = csvToBean.iterator();
+			CsvToBean<T> csvToBean = new CsvToBeanBuilder(reader).withType(className).withIgnoreLeadingWhiteSpace(true)
+			.build();
+			Iterator<T> csvUserIterator = csvToBean.iterator();
 			while (csvUserIterator.hasNext()) {
-				CSVStateCensus state = csvUserIterator.next();
+				T state = csvUserIterator.next();
 				count++;
-				if (state.getState() == null || state.getPopulation() == 0 || state.getAreaInSqKm() == 0) {
-					throw new MyException(MyException.ExceptionType.INCORRECT_HEADER, "Header doesn't match");
+				if (state instanceof CSVStateCensus) { // comparing if read file is of CsvStateCensus class
+					if (((CSVStateCensus) state).getState() == null || ((CSVStateCensus) state).getPopulation() == 0
+							|| ((CSVStateCensus) state).getAreaInSqKm() == 0) {
+						throw new MyException(MyException.ExceptionType.INCORRECT_HEADER, "Header doesn't match");
+					}
+				}
+				if (state instanceof CsvStateCode) { // comparing if read file is of CsvStateCode class
+					if (((CsvStateCode) state).getSrNo() == 0 || ((CsvStateCode) state).getStateName() == null
+							|| ((CsvStateCode) state).getStateCode() == null) {
+
+						throw new MyException(MyException.ExceptionType.INCORRECT_HEADER, "Header doesn't match");
+					}
 				}
 			}
-		}
-		catch (NoSuchFileException e) {
+		} catch (NoSuchFileException e) {
 			if (Sample_CSV_File_Path.contains(".csv")) {
 				throw new MyException(MyException.ExceptionType.FILE_NOT_FOUND, "File not found");
 			}
-			throw new MyException(MyException.ExceptionType.INCORRECT_TYPE, "Incorrect Type");
+			throw new MyException(MyException.ExceptionType.INCORRECT_TYPE, "Incorrect File Type");
 
 		} catch (RuntimeException e) {
 			throw new MyException(MyException.ExceptionType.DELIMITER_NOT_FOUND, "Incorrect Delimiter");
--- src/test/java/com/censusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/censusanalysertest/StateCensusAnalyserTest.java
@@ -2,73 +2,167 @@ package com.censusanalysertest;
 
 import java.io.IOException;
 
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.censusanalyser.MyException;
 import com.censusanalyser.StateCensusAnalyser;
 import com.censusanalyser.CSVStateCensus;
+import com.censusanalyser.CsvStateCode;
 
 public class StateCensusAnalyserTest {
 
+	/**
+	 * This test case pass when a file with 8 rows is read
+	 */
 	@Test
 	public void givenCsvFile_with8rowscomparingwith8_returnstrue() {
 		try {
 			StateCensusAnalyser analyser = new StateCensusAnalyser(
 					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/StateCensusData.csv");
-			Assert.assertEquals(8,analyser.readStateRecordDetail());
+			Assert.assertEquals(8,analyser.readStateRecordDetail(CSVStateCensus.class));
 		} catch (MyException e) {
 			System.out.println(e);
 		}
 	}
 
+	/**
+	 * If given wrong csv file anticipates exception
+	 */
 	@Test
 	public void givenCsvFile_whichIsIncorrect_expectsException() {
 		try {
 			StateCensusAnalyser analyser = new StateCensusAnalyser(
 					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/StateCensusDataType.csv");
-			analyser.readStateRecordDetail();
+			analyser.readStateRecordDetail(CSVStateCensus.class);
 		} catch (MyException e) {
 			Assert.assertEquals("File not found", e.getMessage());
 			System.out.println(e);
 		}
 	}
 
+	/**
+	 * If given file other than csv type, anticipates custom exception
+	 */
 	@Test
-	public void givenCsvFile_whichIsWrongType_returnsFalse() {
+	public void givenCsvFile_whichIsWrongType_expectsException() {
 		try {
 			StateCensusAnalyser analyser = new StateCensusAnalyser(
 					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/AddressBook.txt");
-			analyser.readStateRecordDetail();
+			analyser.readStateRecordDetail(CSVStateCensus.class);
 		} catch (MyException e) {
-			Assert.assertEquals("Incorrect Type", e.getMessage());
+			Assert.assertEquals("Incorrect File Type", e.getMessage());
 			System.out.println(e);
 		}
 	}
 
+	/**
+	 * If csv file with invalid delimiter if given Anticipates custom exception
+	 */
 	@Test
-	public void givenCsvFile_withwrongdelimeter_returnsFalse() {
+	public void givenCsvFile_withwrongdelimiter_expectsException() {
 		try {
 			StateCensusAnalyser analyser = new StateCensusAnalyser(
 					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/InvalidDelimiter.csv");
-			analyser.readStateRecordDetail();
+			analyser.readStateRecordDetail(CSVStateCensus.class);
 		} catch (MyException e) {
 
 			System.out.println(e);
-			Assert.assertEquals("Incorrect Delimeter", e.getMessage());
+			Assert.assertEquals("Incorrect Delimiter", e.getMessage());
 		}
 	}
 
 	@Test
-	public void givenCsvFile_withwrongdHeader_returnsFalse() {
+	public void givenCsvFile_withwrongdHeader_expectsException() {
 		try {
 			StateCensusAnalyser analyser = new StateCensusAnalyser(
 					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/IncorrectHeader.csv");
-			analyser.readStateRecordDetail();
+			analyser.readStateRecordDetail(CSVStateCensus.class);
 		} catch (MyException e) {
 
 			System.out.println(e);
 			Assert.assertEquals("Header doesn't match", e.getMessage());
 		}
+	}
+	//
+	//
+	//
+	//
+	/**
+	 * Test case passes when a csv state code file with 3 rows of data is read
+	 */
+	@Test
+	public void givenStateCodeCsvFile_with3rowscomparingwith3_returnstrue() {
+		try {
+			StateCensusAnalyser analyser = new StateCensusAnalyser(
+					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/stateCode.csv");
+			Assert.assertEquals(3, analyser.readStateRecordDetail(CsvStateCode.class));
+		} catch (MyException e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * Given wrong csv file Anticipates custom exception
+	 */
+	@Test
+	public void givenStateCodeCsvFile_whichIsIncorrect_expectsException() {
+		try {
+			StateCensusAnalyser analyser = new StateCensusAnalyser(
+					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/stateCodewrong.csv");
+			analyser.readStateRecordDetail(CsvStateCode.class);
+		} catch (MyException e) {
+			Assert.assertEquals("File not found", e.getMessage());
+			System.out.println(e);
+		}
+
+	}
+	
+	/**
+	 * If given file other than csv type. Anticipates custom exception
+	 */
+	@Test
+	public void givenStateCodeCsvFile_whichIsWrongType_expectsException() {
+		try {
+			StateCensusAnalyser analyser = new StateCensusAnalyser(
+					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/bookexample.txt");
+			analyser.readStateRecordDetail(CsvStateCode.class);
+		} catch (MyException e) {
+			Assert.assertEquals("Incorrect File Type", e.getMessage());
+			System.out.println(e);
+		}
+	}
+	
+	/**
+	 * If csv file with invalid delimiter if given Anticipates custom exception
+	 */
+	@Test
+	public void givenStateCodeCsvFile_withwrongDelimeter_expectsException() {
+		try {
+			StateCensusAnalyser analyser = new StateCensusAnalyser(
+					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/DelimiterCode.csv");
+			analyser.readStateRecordDetail(CsvStateCode.class);
+		} catch (MyException e) {
+
+			System.out.println(e);
+			Assert.assertEquals("Incorrect Delimiter", e.getMessage());
+		}
+	}
+	
+	/**
+	 * If csv file with invalid header if given Anticipates custom exception
+	 */
+	@Test
+	public void givenStateCodeCsvFile_withwrongdHeader_expectsException() {
+		try {
+			StateCensusAnalyser analyser = new StateCensusAnalyser(
+					"/Users/anirudhasm/Desktop/eclipse-yml_training_workspace/StateCensusAnalyser/resources/InvalidHeaderStateCode.csv");
+			analyser.readStateRecordDetail(CsvStateCode.class);
+		} catch (MyException e) {
+
+			System.out.println(e);
+		}
+		
 	}
 }
